3385f5300b3ec3470d2197a00dfe8e97aa0cfceb
Artem <56472854+Twocram@users.noreply.github.com>
2025-02-22 11:46:56 +0500

    feat: add content for about view

--- src/views/about-view.vue
+++ src/views/about-view.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts"></script>
+
 <template>
-  <div>about view</div>
+  <div class="max-w-2xl mx-auto bg-white p-6 rounded-lg shadow-lg">
+    <h1 class="text-2xl font-bold mb-6 text-center">{{ $t('about.title') }}</h1>
+    <div class="text-center"> {{ $t('about.content') }} </div>
+  </div>
 </template>
 
 <style></style>
--- src/views/settings-view.vue
+++ src/views/settings-view.vue
@@ -19,16 +19,14 @@ function saveSettings() {
 </script>
 
 <template>
-    <div class="min-h-screen bg-gray-100 py-8">
-        <div class="max-w-2xl mx-auto bg-white p-6 rounded-lg shadow-lg">
-            <h1 class="text-2xl font-bold mb-6 text-center">{{ $t('settings.title') }}</h1>
-            <v-select class="mb-6" :options="localeOptions" v-model="currentLocale"
-                :label="$t('settings.chooseLanguage')"></v-select>
+    <div class="max-w-2xl mx-auto bg-white p-6 rounded-lg shadow-lg">
+        <h1 class="text-2xl font-bold mb-6 text-center">{{ $t('settings.title') }}</h1>
+        <v-select class="mb-6" :options="localeOptions" v-model="currentLocale"
+            :label="$t('settings.chooseLanguage')"></v-select>
 
-            <v-button class="w-full" @click="saveSettings">
-                {{ $t('actions.save') }}
-            </v-button>
-        </div>
+        <v-button class="w-full" @click="saveSettings">
+            {{ $t('actions.save') }}
+        </v-button>
     </div>
 </template>
 
