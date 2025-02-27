9a08c178b052b95e8ddd789464ac9c43a8dff8a1
Artem <56472854+Twocram@users.noreply.github.com>
2025-02-27 23:29:58 +0500

    feat: jsdoc for useToast and useHandleKeydown composables

--- src/composables/use-handle-keydown.ts
+++ src/composables/use-handle-keydown.ts
@@ -1,5 +1,10 @@
 import { onMounted, onUnmounted } from 'vue'
 
+/**
+ * Composable function to handle keydown event and call a function when a specified key is pressed.
+ * @param key - the key to listen for
+ * @param fn - the function to call when the key is pressed
+ */
 export function useHandleKeydown(key: string, fn: () => void) {
   onMounted(() => {
     window.addEventListener('keydown', (e) => {
--- src/composables/use-toast.ts
+++ src/composables/use-toast.ts
@@ -1,6 +1,12 @@
 import type { ToastType } from 'vue3-toastify'
 import { toast } from 'vue3-toastify'
 
+/**
+ * Displays a toast notification with the given message and type.
+ *
+ * @param type - The type of toast notification to display.
+ * @param message - The message to display in the toast notification.
+ */
 export function useToast(type: ToastType, message: string): void {
   toast(message, { type })
 }
